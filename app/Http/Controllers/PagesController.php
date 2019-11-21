73a95a9216382fbc5562767dbbd8ee208c750616
WildanKurniadi <danprogramming10@gmail.com>
2019-11-22 01:19:01 +0700

    #09.1

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+//untuk halaman statis saja (home, about, student, faq, etc.)
 class PagesController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('index');
+    }
+
+    public function about()
+    {
+        return view('about', ['nama' => 'Wildan Kurniadi']);
+    }
 }
