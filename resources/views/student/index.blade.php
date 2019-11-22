27de7240593d429defc5263f087637ac9a7f4218
WildanKurniadi <danprogramming10@gmail.com>
2019-11-22 21:30:43 +0700

    #12 Perbaikan View Student

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -6,7 +6,33 @@
     <div class="container">
        <div class="row">
           <div class="col-10">
-             <h1 class="mt-3">Hello Student!!</h1>
+             <h1 class="mt-3">Daftar Mahasiswa</h1>
+
+             <table class="table">
+               <tr>
+                  <thead class="thead-dark">
+                     <th scope="col">#</th> 
+                     <th scope="col">NIM</th> 
+                     <th scope="col">Nama</th> 
+                     <th scope="col">Email</th> 
+                     <th scope="col">Jurusan</th> 
+                     <th scope="col">Aksi</th> 
+                  </thead>
+               </tr>
+               <tbody>
+                  <tr>
+                     <th scope="row">1</th>
+                     <td>Wildan</td>
+                     <td>10113267</td>
+                     <td>dankurniadi10@gmail.com</td>
+                     <td>Teknik Informatika</td>
+                     <td>
+                        <a href="" class="badge badge-warning">Edit</a>
+                        <a href="" class="badge badge-danger">Hapus</a>
+                     </td>
+                  </tr>
+               </tbody>
+             </table>
           </div>
        </div>
     </div>
